bd23f7d7eca83859b90a93cc3717ff4ee015090d
Laszlo Schoonheid <lschoonheid@gmail.com>
2023-06-28 13:48:20 +0200

    Hard negative training and refractoring

--- examples/2nd place/trackml-2-solution-example.py
+++ examples/2nd place/trackml-2-solution-example.py
@@ -23,20 +23,24 @@ prefix = DATA_SAMPLE
 
 
 # Hyperparameters
-# N_EVENTS = 2
-N_EVENTS = 1
+N_EVENTS = 10
 EVENT_OFFSET = 10
 EVENT_RANGE = range(EVENT_OFFSET, EVENT_OFFSET + N_EVENTS)
+
 # Learning rates
 LR_0, LR_1, LR_2 = -5, -4, -5
 LEARNING_RATES = [10 ** (lr) for lr in [LR_0, LR_1, LR_2]]
-# BATCH_SIZE = 8000
 BATCH_SIZE = 8000
 
-CROSS_VALIDATION = 0.05
+# First training
+VALIDATION_SPLIT = 0.05
 E_0, E_1, E_2 = 1, 20, 3
 EPOCHS = [E_0, E_1, E_2]
 LOSS_FUNCTION = "binary_crossentropy"
+
+# Hard negative training
+LR_HARD = [-4, -5, -6]
+EPOCHS_HARD = 30, 10, 2
 
 
 def get_event(event):
@@ -55,6 +59,14 @@ def get_particle_ids(truth):
 
 
 def get_features(hits, cells):
+    """Extract the following features per hit:
+    - x, y, z: coordinates in 3D space
+    - TODO volume_id, layer_id, module_id: detector ID
+    - cell count: number of cells that have fired in this hit
+    - cell value sum: sum of cell values for this hit
+
+
+    """
     # Take #cells hit per hit_id
     hit_cells = cells.groupby(["hit_id"]).value.count().values
     # Take cell value sum per hit_id
@@ -127,6 +139,33 @@ def get_train(event_range=EVENT_RANGE, features=None):
     return Train
 
 
+def init_model(fs=10):
+    model = Sequential()
+    model.add(Dense(800, activation="selu", input_shape=(fs,)))
+    model.add(Dense(400, activation="selu"))
+    model.add(Dense(400, activation="selu"))
+    model.add(Dense(400, activation="selu"))
+    model.add(Dense(200, activation="selu"))
+    model.add(Dense(1, activation="sigmoid"))
+    return model
+
+
+def do_train(model, Train, lr, epochs, batch_size, validation_split, loss_function, callbacks=[], epochs_passed=0):
+    model.compile(loss=[loss_function], optimizer=Adam(learning_rate=10 ** (lr)), metrics=["accuracy"])
+    History = model.fit(
+        x=Train[:, :-1],  # type: ignore
+        y=Train[:, -1],  # type: ignore
+        batch_size=batch_size,
+        initial_epoch=epochs_passed,
+        epochs=epochs + epochs_passed,
+        verbose=2,  # type: ignore
+        validation_split=validation_split,
+        shuffle=True,
+        callbacks=callbacks,
+    )
+    return History
+
+
 def get_hard_negatives(Train, model, event_range=EVENT_RANGE):
     Train_hard = []
     for i in tqdm(event_range):
@@ -134,13 +173,10 @@ def get_hard_negatives(Train, model, event_range=EVENT_RANGE):
         event = "event0000010%02d" % i
         hits, cells, truth, particles = get_event(event)
         features = get_features(hits, cells)
-        particle_ids = get_particle_ids(truth)
 
         # Take all pairs of hits that do not belong to the same track id
-        # TODO this is duplicate code
         size = 30000000
         n = len(truth)
-
         Train0 = get_train_0(size, n, truth, features)
 
         pred = model.predict(Train0[:, :-1], batch_size=20000)
@@ -157,38 +193,54 @@ def get_hard_negatives(Train, model, event_range=EVENT_RANGE):
     return Train_hard
 
 
-def init_model(fs=10):
-    model = Sequential()
-    model.add(Dense(800, activation="selu", input_shape=(fs,)))
-    model.add(Dense(400, activation="selu"))
-    model.add(Dense(400, activation="selu"))
-    model.add(Dense(400, activation="selu"))
-    model.add(Dense(200, activation="selu"))
-    model.add(Dense(1, activation="sigmoid"))
-    return model
+def get_predict(hit, truth, features, thr=0.5):
+    Tx = np.zeros((len(truth), 10))
+    Tx[:, 5:] = features
+    Tx[:, :5] = np.tile(features[hit], (len(Tx), 1))
+    pred = model.predict(Tx, batch_size=len(Tx))[:, 0] # type: ignore
+    # TTA
+    idx = np.where(pred > thr)[0]
+    Tx2 = np.zeros((len(idx), 10))
+    Tx2[:, 5:] = Tx[idx, :5]
+    Tx2[:, :5] = Tx[idx, 5:]
+    pred1 = model.predict(Tx2, batch_size=len(idx))[:, 0] # type: ignore
+    pred[idx] = (pred[idx] + pred1) / 2
+    return pred
 
 
-def do_train(model, Train, lr, epochs, batch_size, cross_validation, loss_function, callbacks=[], epochs_passed=0):
-    model.compile(loss=[loss_function], optimizer=Adam(learning_rate=10 ** (lr)), metrics=["accuracy"])
-    History = model.fit(
-        x=Train[:, :-1],  # type: ignore
-        y=Train[:, -1],  # type: ignore
-        batch_size=batch_size,
-        initial_epoch=epochs_passed,
-        epochs=epochs + epochs_passed,
-        verbose=2,  # type: ignore
-        validation_split=cross_validation,
-        shuffle=True,
-        callbacks=callbacks,
-    )
-    return History
+def test():
+    # Load event
+    event = "event000001001"
+    hits, cells, truth, particles = get_event(event)
+
+    count = hits.groupby(["volume_id", "layer_id", "module_id"])["hit_id"].count().values
+    module_id = np.zeros(len(hits), dtype="int32")
+
+    for i in range(len(count)):
+        si = np.sum(count[:i])
+        module_id[si : si + count[i]] = i
+
+    # Define test input
+    features = get_features(hits, cells)
 
 
 def _get_log_dir():
     return DIRECTORY + "training_logs/2nd_place_example/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 
-if __name__ == "__main__":
+def _get_tensorboard_callback():
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=_get_log_dir(), histogram_freq=1)
+    return tensorboard_callback
+
+
+def run_training(
+    learning_rates=LEARNING_RATES,
+    epochs=EPOCHS,
+    learning_rates_hard=LR_HARD,
+    epochs_hard=EPOCHS_HARD,
+    batch_size=BATCH_SIZE,
+    validation_split=VALIDATION_SPLIT,
+):
     # Prepare training set
     Train = get_train()
     # Init model
@@ -199,19 +251,55 @@ if __name__ == "__main__":
     # Train model
 
     # Prerpare tensorboard
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=_get_log_dir(), histogram_freq=1)
+    tensorboard_callback = _get_tensorboard_callback()
+
     # Train regular
-    for lr, epochs in zip(LEARNING_RATES, EPOCHS):
+    for lr, epochs in zip(learning_rates, epochs):
         # Execute training
         do_train(
             model,
             Train,
             lr,
             epochs,
-            BATCH_SIZE,
-            CROSS_VALIDATION,
+            batch_size,
+            validation_split,
             LOSS_FUNCTION,
             epochs_passed=epochs_passed,
             callbacks=[tensorboard_callback],
         )
         epochs_passed += epochs
+
+    # Add hard negatives to training set
+    Train_hard = get_hard_negatives(Train, model)
+    Train = np.vstack((Train, Train_hard))
+    np.random.shuffle(Train)
+    print(Train.shape)
+
+    # Train hard
+    for lr, epochs in zip(learning_rates_hard, epochs_hard):
+        # Execute training
+        do_train(
+            model,
+            Train,
+            lr,
+            epochs,
+            batch_size,
+            validation_split,
+            LOSS_FUNCTION,
+            epochs_passed=epochs_passed,
+            callbacks=[tensorboard_callback],
+        )
+        epochs_passed += epochs
+
+    return model
+
+
+if __name__ == "__main__":
+    new_model = True
+
+    if new_model:
+        model = run_training()
+    else:
+        model = load_model("../input/trackml/my_model.h5")
+
+    test()
